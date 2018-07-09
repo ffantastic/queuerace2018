7f07683201860672ec7e77a88f5c3b40b2dfaf60
Wentao Fan <wenfan@microsoft.com>
2018-07-09 19:35:43 +0800

    15 bucket, 400 chunk size

--- src/main/java/io/openmessaging/IndexChunk.java
+++ src/main/java/io/openmessaging/IndexChunk.java
@@ -10,7 +10,7 @@ class IndexChunk {
     public String IndexFilename;
     private int[] offsets;
     private int indexOffset;
-    private static final int CHUNK_SIZE = 300;
+    private static final int CHUNK_SIZE = 400;
 
     public IndexChunk(int msgStartPos, String filename) {
         this.Filename = filename;
